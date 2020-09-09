a831773d7657432f713ba0f826c1324be2d8a164
Amir Servati <amirservatiasl@gmail.com>
2020-09-09 23:07:50 +0430

    Link Editing added #16

--- mehrabad.py
+++ mehrabad.py
@@ -8,36 +8,32 @@ TOKEN = "992894946:AAHrFRfhetVL4dhlE_xjFJgJ75VWDYRC_ss"
 PORT = int(os.environ.get('PORT', '5000'))
 CHANNEL_LINK = "[Mehrabad Airport](https://t.me/mehrabad_airport)"
 
-def caption(bot, update):
-    message_id  = update.effective_message.message_id
-    try:
-        #plain text
-        content     = update.effective_message.text
-        bot.sendMessage(text=content,
-                        chat_id="@amirstestchannel",
-                        parse_mode=ParseMode.MARKDOWN)
-    except:
-        content     = update.effective_message.caption
+def message(bot, update):
+    message_id  = update.message.message_id
+    content     = update.message.text
     if "Mehrabad Airport" in content:
         content = content.replace("Mehrabad Airport", CHANNEL_LINK)
 
     else:
         content = content + "\n\n" + CHANNEL_LINK
 
-    try:
-        bot.editMessageText(text=content,
+    bot.editMessageText(text=content,
                         chat_id="@amirstestchannel",
                         message_id=message_id,
                         parse_mode=ParseMode.MARKDOWN)
-    except:
-        bot.editMessageCaption(caption=content,
+
+def caption(bot, update):
+    message_id   = update.message.message_id
+    message_type = update.message.document.mime_type
+    bot.sendMessage(text=message_type,
                         chat_id="@amirstestchannel",
-                        message_id=message_id,
                         parse_mode=ParseMode.MARKDOWN)
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 
-dispatcher.add_handler(MessageHandler(Filters.all, caption))
+dispatcher.add_handler(MessageHandler(Filters.text, message))
+dispatcher.add_handler(MessageHandler(Filters.document.category("image"), caption))
+dispatcher.add_handler(MessageHandler(Filters.document.category("video"), caption))
 
 
 #updater.start_polling()
